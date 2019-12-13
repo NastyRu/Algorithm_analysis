f6479ada5239c72dc5ca05a7d954e8b15a544627
sag17u762 <nasty.sidenko@mail.ru>
2019-12-13 16:47:37 +0300

    Added report

--- lab7/main.py
+++ lab7/main.py
@@ -4,7 +4,9 @@ def shift_array(x):
         j = d[i - 1]
         while j > 0 and x[j] != x[i]:
             j = d[j - 1]
-        d[i] = j + 1
+        if x[j] == x[i]:
+            j += 1
+        d[i] = j
     return d
 
 def KMP(s, x):
@@ -25,12 +27,13 @@ def KMP(s, x):
 
 def jump_array(x):
     d = [len(x) for i in range(256)]
-    new_x = x[::-1]
+    new_x = x[:-1]
+    new_x = new_x[::-1]
 
     for i in range(len(new_x)):
-        d[ord(new_x[i])] = i
+        if (d[ord(new_x[i])] == 6):
+            d[ord(new_x[i])] = i + 1
     return d
-
 
 def BM(s, x):
     d = jump_array(x)
@@ -50,13 +53,20 @@ def BM(s, x):
         return -1
 
 def main():
-    print(ord('z'))
     print("Введите строку 1")
     s1 = input()
     print("Введите строку 2")
     s2 = input()
-    print(KMP(s1, s2))
-    print(BM(s1, s2))
+    n1 = KMP(s1, s2)
+    if (-1 == n1):
+        print("KMP: Подстрока не найдена")
+    else:
+        print("KMP: " + str(n1))
+    n2 = BM(s1, s2)
+    if (-1 == n2):
+        print("BM: Подстрока не найдена")
+    else:
+        print("BM: " + str(n2))
 
 if __name__ == "__main__":
     main()
